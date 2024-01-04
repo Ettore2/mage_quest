d09f514c050032720bad77c3a112e9599dce87ef
Ettore2 <114524494+Ettore2@users.noreply.github.com>
2024-01-04 19:40:39 +0100

    change level 23

--- app/src/main/java/com/example/gamequest/LevelsManager.java
+++ app/src/main/java/com/example/gamequest/LevelsManager.java
@@ -257,7 +257,7 @@ public class LevelsManager {
                     "E1,E1_P1,E1,E1,E1_M1,E1_M1,X1,X1,E1,E1_C1",
             "level 23\n" +
                     "level 23\n" +
-                    "1 2 4 1 5 1\n" +
+                    "1 2 4 1 5 2\n" +
                     "6 2 4\n" +
                     "X1,X1,X1,X1,E1,E1,X1,X1,X1,X1,X1,X1,X1,X1,X1,\n" +
                     "X1,X1,X1,X1,E1,E1,X1,X1,X1,X1,X1,X1,X1,X1,X1,\n" +
@@ -267,7 +267,7 @@ public class LevelsManager {
                     "E1,E1_C1,X1,E1,E1,E1,E1,E1,E1,E1,E1,E1,E1,E1,G1,\n" +
                     "E1,X1,E1,E1,E1,E1,E1_C1,E1,E1,E1,E1,E1,E1,E1,G1,\n" +
                     "E1,E1_P1,E1,E1,E1,E1,X1,E1,E1,E1,E1,E1,E1,E1_C1,X1,\n" +
-                    "X1,X1,X1,E1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,X1,X1,X1",
+                    "X1,X1,X1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,E1_U1,X1,X1,X1",
             "level 24\n" +
                     "level 24\n" +
                     "1 -1 2 5 4 1\n" +
