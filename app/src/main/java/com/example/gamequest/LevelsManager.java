fb90546c07355f7bf63cf4e3e82034a59cdb5580
Ettore2 <114524494+Ettore2@users.noreply.github.com>
2024-01-04 15:53:10 +0100

    add level 27

--- app/src/main/java/com/example/gamequest/LevelsManager.java
+++ app/src/main/java/com/example/gamequest/LevelsManager.java
@@ -311,7 +311,21 @@ public class LevelsManager {
                     "E1,E1,E1,E1,E1,E1,E1,E1,E1,E1,E1,X1,X1,E1,E1,E1,E1,E1,X1,E1,E1,E1,E1,E1,\n" +
                     "E1,E1_P1,E1,E1,E1,E1,E1,E1,E1,E1,E1,X1,X1,E1,E1,E1,E1,E1,E1,X1,E1,E1,E1,E1,\n" +
                     "X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,E1,E1,E1,E1,E1,\n" +
-                    "X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,E1,E1,E1,E1,E1"
+                    "X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,E1,E1,E1,E1,E1",
+            "level 27\n" +
+                    "level 27\n" +
+                    "2 -1 4 2\n" +
+                    "8 2 6\n" +
+                    "X1,X1,X1,E1_C1,E1,X1,X1,X1,X1,X1,E1_C1,E1,E1_D1,E1,E1,X1,X1,E1_C1,\n" +
+                    "X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,E1,E1,E1,E1,X1,\n" +
+                    "X1,X1,X1,X1,E1,E1,E1,E1,X1,E1,X1,E1,E1,E1,E1,X1,X1,E1,\n" +
+                    "X1,E1_C1,X1,E1,E1,E1,X1,E1,X1,X1,E1,E1,E1,E1,X1,X1,X1,E1,\n" +
+                    "X1,X1,X1,E1,X1,X1,E1,X1,X1,E1,E1,E1,E1,E1,X1,X1,E1,E1,\n" +
+                    "X1,X1,X1,X1,E1,E1,X1,X1,E1,E1,E1_M1,E1,E1,X1,X1,E1,X1,X1,\n" +
+                    "X1,X1,X1,E1,E1,X1,X1,X1,X1,X1,X1,X1,X1,X1,X1,E1,X1,X1,\n" +
+                    "X1,E1,X1,E1,X1,X1,X1,X1,X1,E1,X1,E1,E1,X1,X1,E1,X1,X1,\n" +
+                    "E1,X1,X1,X1,X1,X1,X1,E1_C1,X1,E1,X1,E1,E1,X1,X1,E1_C1,X1,X1,\n" +
+                    "E1,E1_P1,X1,X1,X1,X1,X1,X1,X1,E1,X1,X1,X1,X1,X1,X1,X1,X1"
     };
 
     public static class Level{
